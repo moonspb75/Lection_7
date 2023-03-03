31ab01c6f5e065c0f4757bc6034e3e8215e3820f
Yanina <moonspb75@mail.ru>
2023-03-03 22:01:59 +0300

    задача 51

---
+++ task_51/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] ArrayWithRandom(int row, int column)
+{
+    int[,] arr = new int[row, column];
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1, 10);
+        }
+        
+    }
+    return arr;
+}
+
+int Sum(int[,] matrix)
+{
+    int sum = 0;
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum = sum + matrix[i,j];
+            }
+            
+        }
+    }
+    return sum;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = ArrayWithRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.Write($" {Sum(matrix)}");
