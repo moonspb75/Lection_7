e711a24d177dc70573a1765167be2f5b9daa64ee
Yanina <moonspb75@mail.ru>
2023-03-03 22:01:19 +0300

    задача 49

---
+++ task_49/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+
+int[,] ArrayWithRandom(int row, int column)
+{
+    int[,] arr = new int[row, column];
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1, 10);
+        }
+        
+    }
+    return arr;
+}
+
+int [,] Rewrite(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i,j] = matrix[i, j] * matrix[i, j];
+            
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = ArrayWithRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(Rewrite(matrix));
